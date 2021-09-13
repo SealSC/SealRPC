b626d005dea40403e669fbec8a5767e8e899c632
ltto <121172722@qq.com>
2021-09-13 09:38:51 +0800

    fix examples func not found

--- examples/client.go
+++ examples/client.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	client := SealRPC.NewEthRPCClient("http://127.0.0.1:8080")
+	client := SealRPC.NewJsonRPCClient("http://127.0.0.1:8080")
 	number, err := client.EthGetBlockByNumber("0x1", true)
 	fmt.Println(number, err)
 	index, err := client.EthGetBlockTransactionCountByHash("0xe9dc52ef255d3b8ee11b35acc4b25f41ce096c71392aafe58907df9e1dbaaa4b")
